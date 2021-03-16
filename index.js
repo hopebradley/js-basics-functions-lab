cbdf647c2690d01891bbfcd592a53743a58c09fb
Hope Bradley <hope.e.bradley@gmail.com>
2021-03-16 10:29:26 -0400

    commit tree b48135d270acca13a783ce8879906f83d8cc7650

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(block) {
+  if (block>42) {
+    return block-42;
+  }
+  else {
+    return 42-block;
+  }
+
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block)*264;
+}
+
+function distanceTravelledInFeet(start, end) {
+
+  const blocks =  Math.max(start, end)-Math.min(start, end);
+  return blocks*264;
+}
+
+function calculatesFarePrice(start, end) {
+  const feetTravelled = distanceTravelledInFeet(start, end);
+  if (feetTravelled < 401) {
+    return 0;
+  }
+  else if (feetTravelled > 400 && feetTravelled < 2001) {
+    return (feetTravelled-400)*.02;
+  }
+  else if (feetTravelled < 2501) {
+    return 25;
+  }
+  else {
+    return 'cannot travel that far';
+  }
+}
+
+
+
+
+
+
+
